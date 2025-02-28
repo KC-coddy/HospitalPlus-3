3829a7c0f780a435a5662bf14687963faba632ec
KC-coddy <mariocastro1738@GMAIL.COMgit config --global user.email mariocastro1738@gmail.com>
2025-02-28 16:27:34 +0300

    Another Commit

---
+++ hospitalapp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from hospitalapp.models import *
+
+# Register your models here.
+admin.site.register(Patient)
+admin.site.register(Doctor)
+admin.site.register(Staff)
+admin.site.register(ward)
+admin.site.register(Appointment)
+admin.site.register(contact)
