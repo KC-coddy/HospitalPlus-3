3be95313fd3afe32d13aaba1f1c7f10674ef839f
KC-coddy <mariocastro1738@GMAIL.COMgit config --global user.email mariocastro1738@gmail.com>
2025-03-04 16:16:40 +0300

    first commit

--- hospitalapp/views.py
+++ hospitalapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from hospitalapp.models import*
 
 
@@ -24,9 +24,9 @@ def departments(request):
 def doctors(request):
     return render(request,'doctors.html')
 
-def appoint(request):
+def appointment(request):
    if request.method == "POST":
-       myappointments=Appointment(
+       myappointment=Appointment(
            name = request.POST['name'],
            email = request.POST['email'],
            phone = request.POST['phone'],
@@ -36,10 +36,11 @@ def appoint(request):
            message = request.POST['message'],
 
        )
-       myappointments.save()
-       return redirect('/appointment')
+       myappointment.save()
+       return redirect('/show')
    else:
        return render(request,'appointment.html')
+
 
 def contact(request):
     if request.method == "POST":
@@ -51,5 +52,35 @@ def contact(request):
 
 
         )
+        mycontact.save()
+        return redirect('contact')
 
+    else:
+        return render(request, 'contact.html')
+
+
+def show(request):
+    all= Appointment.objects.all()
+    return render(request,'show.html',{'all':all})
+
+
+def delete(request,id):
+    deleteappointment =Appointment.objects.get(id=id)
+    deleteappointment.delete()
+    return redirect('show')
+def edit(request,id):
+    appointment= get_object_or_404(Appointment,id=id)
+    if request.method == "POST":
+        appointment.name=request.POST['name']
+        appointment.email=request.POST['email']
+        appointment.phone=request.POST['phone']
+        appointment.date=request.POST['date']
+        appointment.department=request.POST['department']
+        appointment.doctor=request.POST['doctor']
+        appointment.message=request.POST['message']
+        appointment.save()
+        return redirect('/show')
+
+    else:
+        return render(request,'edit.html',{'appointment':appointment})
 
